8ed822657c81c2c0fb9d97d8bc87507fa8801c99
binyoma <innocentbinyoma@pm.me>
2024-07-03 22:04:57 +0200

    named autowiring

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,6 +4,7 @@
 namespace App\Service ;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper 
@@ -13,16 +14,21 @@ class MarkdownHelper
     private $markdownParser;
     private $cache;
     private $isDebug;
+    private $logger;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug )
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger )
     {
       $this->markdownParser= $markdownParser;
       $this->cache =$cache; 
       $this->isDebug =$isDebug; 
-      dump($isDebug);
+      $this->logger =$markdownLogger;
+      
     }
 
     public function parse(string $source) : string {
+        if (stripos($source, 'cat') !== false) {
+           $this->logger->info('Meow!');
+        }
         if  ($this->isDebug ) {
             return $this->markdownParser->transformMarkdown($source);
         }
